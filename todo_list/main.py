dc7f21c0ba31047af705db762dab2e26d558caf1
KevinSegovia-dev <kevinsegovia@gmail.com>
2026-08-31 09:54:46 -0500

    Update the main file of todo_list.

--- todo_list/main.py
+++ todo_list/main.py
@@ -7,39 +7,44 @@ class CLI:
     def __init__(self):
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument("-new", "--new", nargs=2, metavar=("name", "description"), help="Create a new task with two arguments")
-        
+
         self.parser.add_argument("-show", "--show", help="View all tasks")
-        
-        self.parser.add_argument("-uptade", "--uptade", type=int, help="ID of the task to edit")
-        
+
+        self.parser.add_argument("-update", "--update", type=int, help="ID of the task to edit")
+
         self.parser.add_argument("-name", "--name", type=str, help="New task name")
         self.parser.add_argument("-description", "--description", type=str, help="New task description")
         self.parser.add_argument("-status", "--status", choices=["not started", "in progress", "completed"], help="New task state")
-        
+
         self.parser.add_argument("-delete", "--delete", type=int, help="Delete a task by ID")
 
-    def new_task(self):
+    def command_new_task(self):
         self.args = self.parser.parse_args()
         if self.args.new:
             name, description = self.args.new
-            
+
         return name, description
-    
-    def show_tasks(self):
+
+    def command_show_tasks(self):
         pass
     
-    def edit_tasks(self):
+    def command_update_task(self):
         self.args = self.parser.parse_args()
-        
-        if self.args.uptade and not (self.args.description,self.args.name,self.args.status):
-            self.parser.error("-edit requires at least 1 argument to change (-name, -description, etc.)")
+
+        id_task = self.args.update
+        new_name = self.args.name
+        new_description = self.args.description
+        new_status = self.args.status
+
+        if self.args.update and not (new_name, new_description, new_status):
+            self.parser.error("-update requires at least 1 argument to change (-name, -description, etc.)")
             
-        if self.args.name is None and self.args.description is None and self.args.status is None:
+        if new_name is None and new_description is None and new_status is None:
             self.parser.error("You must specify at least one field to edit (-name, -description, etc.)")
         
-        return "Successful action"
+        return (id_task,new_name,new_description,new_status)
     
-    def delete_tasks(self):
+    def command_delete_task(self):
         pass
 
 class TaskManager:
@@ -60,5 +65,5 @@ class JsonRepository:
         pass
 
 cli = CLI()
-result = cli.edit_tasks()
+result = cli.command_update_task()
 print(result)
